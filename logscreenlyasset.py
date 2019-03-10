6660f66e277f224051b3d5f8afe29e25f92d92ac
invchar <16106734+invchar@users.noreply.github.com>
2019-03-10 13:54:13 -0500

    update params

--- logscreenlyasset.py
+++ logscreenlyasset.py
@@ -7,10 +7,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Create play log for asset from dump of playlists from Screenly API')
 parser.add_argument('--screenlymanagementpath')
 parser.add_argument('--dumppath')
+parser.add_argument('--assettitle')
 args = parser.parse_args()
 
 screenlymanagementpath = vars(args)['screenlymanagementpath']
 dumppath = vars(args)['dumppath']
+assettitle = vars(args)['assettitle']
 
 
 subprocess.call("{}screenlymanagement.py --action dumpPlaylists --filepath {}".format(screenlymanagementpath, dumppath), shell=True)
@@ -24,7 +26,7 @@ print('Playlist log')
 print('Generated ' + str(datetime.datetime.now()))
 
 for playlist in playlists:
-	if any(a['title'] == 'Army04.png' for a in playlist['assets']):
+	if any(a['title'] == assettitle for a in playlist['assets']):
 		total_time = 0.0
 		for asset in playlist['assets']:
 			total_time += asset['duration']
